467c4e063eba464e34b151d80bf65ad94eada832
Quentin Little <q10@areUXperienced.com>
2013-04-11 13:11:37 -0700

    blurts vomit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Filthytester::Application.config.secret_token = '840f4b676e855eda6f2c0b706691dd405db9715316badd21c3dab5f2c5bc972ff50c5b1de54a8a345ce7e14ad9c04a5bb2596bfeea92577356257870a2be5430'
